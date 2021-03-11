b43eaeb650b6e37a6e28d77fbee96b30df35106a
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-03-11 10:42:36 +1000

    Refactor temperatures.py to use functions.

--- Prac_3/temperatures.py
+++ Prac_3/temperatures.py
@@ -1,22 +1,34 @@
+def main():
+    print_menu()
+    calculator()
 
-MENU = """C - Convert Celsius to Fahrenheit
+
+def print_menu():
+    global MENU
+    MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
-
-    else:
-        print("Invalid option")
     print(MENU)
+
+
+def calculator():
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius * 9.0 / 5 + 32
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = 5 / 9 * (fahrenheit - 32)
+            print("Result: {:.2f} C".format(celsius))
+
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+main()
