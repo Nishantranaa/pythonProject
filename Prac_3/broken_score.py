32ec33a9d3eda92884b01e5840f1a1cb654dba7d
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-03-11 11:03:51 +1000

    Added user input return in the main function

---
+++ Prac_3/broken_score.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+The intention is that the score must be between 0 and 100
+inclusive; 90 or more is excellent; 50 or more is a pass;
+below 50 is bad.
+Be very careful of your boundary conditions... and test!
+"""
+
+def main():
+    score = float(input("Enter score: >>>"))
+    checking_value(score)
+
+
+def checking_value(score):
+    if score < 0 or score > 100:
+        print("Invalid Score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
