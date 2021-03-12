58b514e8ddcb5bc68d2919aaceec5397760e6da2
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-03-12 11:10:09 +1000

    New code to generate a random word based off a vowel or consonant.

---
+++ Prac_3/word_generator.py
@@ -0,0 +1,20 @@
+
+#Random word generator - based on format of words
+
+#Another way to get just consonants would be to use string.ascii_lowercase
+#(all letters) and remove the vowels.
+
+import random
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+word_format = "ccvcvvc"
+word = ""
+for kind in word_format:
+    if kind == "c":
+        word += random.choice(CONSONANTS)
+    else:
+        word += random.choice(VOWELS)
+
+print(word)
