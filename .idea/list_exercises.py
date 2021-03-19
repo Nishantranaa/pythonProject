b9f03156b44f2c9a71ed57fefd3c0dcc2226f7cb
Nishantranaa <nishant.rana@my.jcu.edu.au>
2021-03-19 16:31:39 +1000

    Primary file

---
+++ .idea/list_exercises.py
@@ -0,0 +1,9 @@
+def numbers_to_list(): # this function will ask the user to input 5 numbers and will then add them to a variable called
+# numbers.
+
+numbers = []
+count = 5
+for i in count(1, 5 +1):
+
+    users_input = print(input("Number: "))
+    numbers.append(users_input)
